90c335ca59665d46d1a26cd55cc05becc16bfc46
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-03-10 12:45:29 +0000

    Simple test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.printf("Hello, World!%n");
+
+        Function<Integer,Integer>multiplyByThree=x->x*3;
+        Function<Integer,Integer>plusTen=x->x+10;
+        Function<Integer,Integer>convertToSquare=x->x*x;
+        Function<Integer,Integer>functionResult=multiplyByThree.andThen(plusTen).andThen(convertToSquare);
+        System.out.println(functionResult.apply(4));
+
+        Predicate<String> isNumber = s -> s.matches("-?\\d+(\\.\\d+)?");
+        Predicate<String>isPalindrom=s->new StringBuilder(s).reverse().toString().equals(s);
+        System.out.println(isNumber.and(isPalindrom).test("121"));
+        System.out.println(isNumber.and(isPalindrom).test("Hello"));
+
+        Supplier<LocalDateTime>localDateTime=()->LocalDateTime.now();
+        Supplier<Integer>randomNumber=()->new Random().nextInt();
+        System.out.println(localDateTime.get());
+        System.out.println(randomNumber.get());
+
+        Consumer<String>toLowerCase=s->System.out.println(s.toLowerCase());
+        Consumer<String>toUpperCase=s->System.out.println(s.toUpperCase());
+        toLowerCase.accept("Vlad");
+        toUpperCase.accept("Lol");
     }
 }
